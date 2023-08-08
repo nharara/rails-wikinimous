1474957ada64d8e7d8cc26bf243115954d13ecb5
Nozomu Hara <slipper-spells-0j@icloud.com>
2023-08-08 21:55:55 +0900

    finished wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,9 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   Character.create(name: "Luke", movie: movies.first
+require 'faker'
+
+10.times do
+  Article.create(title: Faker::JapaneseMedia::StudioGhibli.movie, content: Faker::JapaneseMedia::StudioGhibli.quote)
+end
